c7887ac07df86a5cd35e96451c476f3a6bbf1175
kooryan <kooryan03@gmail.com>
2022-09-05 14:48:37 -0500

    blog

---
+++ sections/publications.js
@@ -0,0 +1,11 @@
+export default function Publications() {
+    return (
+        <section className="publication-section" >
+            <h2>
+                Interests
+            </h2>
+            
+        </section>
+        
+    )
+}
